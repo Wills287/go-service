6c80324d7b5211c597f01ff1784fec33a07ffc6e
Mark Williams <Mark.Williams@informa.com>
2020-04-25 13:40:34 +0100

    feat: add methods for retrieving, updating and deleting users

--- model/user.go
+++ model/user.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"errors"
+	"fmt"
+)
+
 /*
 User object.
 */
@@ -25,8 +30,49 @@ func GetUsers() []*User {
 AddUser adds a user using the next available ID.
 */
 func AddUser(user User) (User, error) {
+	if user.ID != 0 {
+		return User{}, errors.New("New User must not have an ID assigned")
+	}
 	user.ID = nextID
 	nextID++
 	users = append(users, &user)
 	return user, nil
+}
+
+/*
+GetUserByID retrieves a user by referencing the ID.
+*/
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+/*
+UpdateUser updates a user by matching the user ID.
+*/
+func UpdateUser(user User) (User, error) {
+	for i, u := range users {
+		if u.ID == user.ID {
+			users[i] = &user
+			return user, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", user.ID)
+}
+
+/*
+RemoveUserByID removes a user referenced by the given ID.
+*/
+func RemoveUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
