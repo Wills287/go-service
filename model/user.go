5b3c1fc6b6e9575d32b4f84969ce5ec80a879f2c
Mark Williams <Mark.Williams@informa.com>
2020-04-25 10:01:41 +0100

    feat: add user methods

--- model/user.go
+++ model/user.go
@@ -13,3 +13,20 @@ var (
 	users  []*User
 	nextID = 1
 )
+
+/*
+GetUsers returns all users.
+*/
+func GetUsers() []*User {
+	return users
+}
+
+/*
+AddUser adds a user using the next available ID.
+*/
+func AddUser(user User) (User, error) {
+	user.ID = nextID
+	nextID++
+	users = append(users, &user)
+	return user, nil
+}
